2f8e79b7ea9f8164e226826b3d042bd769595878
洪莞城 <guancheng.hong@enai.im>
2016-02-25 23:29:38 +0800

    update

---
+++ Gruntfile.js
@@ -0,0 +1,59 @@
+/*
+ * grunt-base-tag
+ * 
+ *
+ * Copyright (c) 2016 CCG
+ * Licensed under the MIT license.
+ */
+
+'use strict';
+
+module.exports = function (grunt) {
+  // load all npm grunt tasks
+  require('load-grunt-tasks')(grunt);
+
+  // Project configuration.
+  grunt.initConfig({
+    jshint: {
+      all: [
+        'Gruntfile.js',
+        'tasks/*.js',
+        '<%= nodeunit.tests %>'
+      ],
+      options: {
+        jshintrc: '.jshintrc',
+        reporter: require('jshint-stylish')
+      }
+    },
+
+    // Before generating any new files, remove any previously-created files.
+    clean: {
+      tests: ['tmp']
+    },
+
+    // Configuration to be run (and then tested).
+    base_tag: {
+      options:{
+        basePath: 'http://statics.xiaoenai.com/street/h5/lovingbox/'
+      },
+      dist: ['test/fixtures/*.html']
+    },
+
+    // Unit tests.
+    nodeunit: {
+      tests: ['test/*_test.js']
+    }
+
+  });
+
+  // Actually load this plugin's task(s).
+  grunt.loadTasks('tasks');
+
+  // Whenever the "test" task is run, first clean the "tmp" dir, then run this
+  // plugin's task(s), then test the result.
+  grunt.registerTask('test', ['clean', 'base_tag', 'nodeunit']);
+
+  // By default, lint and run all tests.
+  grunt.registerTask('default', ['jshint', 'test']);
+
+};
---
+++ tasks/base_tag.js
@@ -0,0 +1,81 @@
+/*
+ * grunt-base-tag
+ * 
+ *
+ * Copyright (c) 2016 CCG
+ * Licensed under the MIT license.
+ */
+
+'use strict';
+
+var path = require('path');
+
+module.exports = function (grunt) {
+
+  // Please see the Grunt documentation for more information regarding task
+  // creation: http://gruntjs.com/creating-tasks
+
+  grunt.registerMultiTask('base_tag', 'add base tag', function () {
+
+    // Merge task-specific and/or target-specific options with these defaults.
+    var options = this.options({
+      basePath: ''
+    });
+
+    var basePath = options.basePath;
+    var titleTag = /<title>/;
+    var baseTag = /<base/;
+
+    // Iterate over all specified file groups.
+    this.files.forEach(function (file) {
+      // Concat specified files.
+      var src = file.src.filter(function (filepath) {
+        // Warn on and remove invalid source files (if nonull was set).
+        if (!grunt.file.exists(filepath)) {
+          grunt.log.warn('Source file "' + filepath + '" not found.');
+          return false;
+        } else {
+          return true;
+        }
+      }).map(function (filepath) {
+        // Read file source.
+        
+        var originalFileContent = grunt.file.read(filepath);
+
+        if(baseTag.test(originalFileContent)){
+          return;
+        }
+
+        var originalFileArr = originalFileContent.split(grunt.util.normalizelf('\n'));
+
+        originalFileArr.forEach(function(value, index, arr){
+          if(titleTag.test(value)){
+
+            var blank = value.split("<title>")[0];
+            arr[index] = value + 
+                         grunt.util.normalizelf('\n') + 
+                         blank + 
+                         '<base href="'+basePath+'">';
+            return;
+          }
+        })
+
+        originalFileContent = originalFileArr.join(grunt.util.normalizelf('\n'));
+
+        grunt.file.write(filepath, originalFileContent);
+        grunt.log.writeln('File "'+filepath+'" created.');
+      })
+    });
+  });
+
+};
+
+
+
+
+
+
+
+
+
+
